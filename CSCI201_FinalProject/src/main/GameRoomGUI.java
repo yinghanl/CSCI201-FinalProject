5782b8838400133fc5f07f7ced12cd92bdde933a
Joshua Lum <joshuanl@usc.edu>
2015-04-08 23:34:52 -0700

    added chatserver and client inner classes to GameRoomGUI, see comment block

--- CSCI201_FinalProject/src/main/GameRoomGUI.java
+++ CSCI201_FinalProject/src/main/GameRoomGUI.java
@@ -1,11 +1,23 @@
 package main;
 
+/*
+ * need to find out how to send objects over server
+ * chat doesnt work with server
+ * need to try to make a new thread for the server 
+*/
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
@@ -42,17 +54,20 @@ public class GameRoomGUI extends JFrame{
 	private JTable playersJT;
 	private JLabel playerLabels[][];
 	private Player players_array[];
-	private Player host;
+	private Player player;
+	private boolean isHost;
+	private String IPAddress;
+	private Chatserver chatserver;
+	private Chatclient chatclient;
 	
-	public GameRoomGUI(Player host){
+	public GameRoomGUI(Player player, boolean isHost, String IPAddress){
 		super("Game Room");
 		setSize(700, 500);
 		setLocation(300, 50);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
-		this.host = host;
-		players_array = new Player[4];
-		players_array[0] = host;
+		this.player = player;
+		this.isHost = isHost;
 		/*
 		 * User who creates the room will have the ready button disabled because they have the start button
 		 * Assumes the host is always ready so boolea[0] is set at true
@@ -60,10 +75,10 @@ public class GameRoomGUI extends JFrame{
 		 */
 		
 		//test players
-		for(int i=1; i<4; i++){
-			players_array[i] = new Player("p"+i);
-		}
-		
+//		for(int i=1; i<4; i++){
+//			players_array[i] = new Player("p"+i);
+//		}
+//		
 		players_ready_array = new boolean[]{true, false, false, false};
 		players_in_room = 1;
 		
@@ -81,8 +96,37 @@ public class GameRoomGUI extends JFrame{
 		
 		add(centerPanel, BorderLayout.CENTER);
 		getRootPane().setDefaultButton(sendButton);
+		
 		setVisible(true);
+		if(isHost){
+			SetupHost();
+			SetupChatClient();
+		}
+		SetupChatClient();
+		
 	}//end of constructor
+	
+	
+	//setup chat server here
+	public String GetIPAddress(){
+		return IPAddress;
+	}
+	
+	public void PlayerConnected(){
+		
+		
+	}//end of player connected to host game room
+	
+	public void SetupHost(){
+		players_array = new Player[4];
+		players_array[0] = player;
+		IPAddress = "localhost";
+		chatserver = new Chatserver();
+	}//end of setuphost
+	
+	public void SetupChatClient(){
+		chatclient = new Chatclient(IPAddress);
+	}//end of setting up the chat client
 	
 	public void createJTextField(){
 		typefield = new JTextField("Press Enter to Send", 30);
@@ -97,7 +141,8 @@ public class GameRoomGUI extends JFrame{
 		sendButton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent ae){
 				String temp = typefield.getText();
-				chatbox.append(temp+"\n");
+				typefield.setText("");
+				chatbox.append("\n"+player.getName()+": "+temp);
 			}
 		});
 		buttonPanel.add(typefield);
@@ -151,14 +196,15 @@ public class GameRoomGUI extends JFrame{
 		jl = new JLabel("  Status");
 		jl.setBorder(border);
 		jp.add(jl);
-		jl = new JLabel("   "+host.getName());
+		jl = new JLabel("   "+player.getName());
 		jl.setBorder(border);
 		jp.add(jl);
 		jl = new JLabel("   Ready");
 		jl.setBorder(border);
 		jp.add(jl);
 		for(int i=1; i<4; i++){
-			jl = new JLabel("   "+players_array[i].getName());
+			//jl = new JLabel("   "+players_array[i].getName());
+			jl = new JLabel("   player"+i);
 			jl.setBorder(border);
 			jp.add(jl);
 			jl = new JLabel("   Not Ready");
@@ -166,7 +212,96 @@ public class GameRoomGUI extends JFrame{
 			jp.add(jl);
 		}//end of for
 		centerTopPanel.add(jp);
-	}//end of creating the panel that holds a table of players
+	}//end of creating the panel that holds a table of player
 	
 	
+	public class Chatserver {
+		private ServerSocket ss;
+		private Socket s;
+		private BufferedReader br;
+		private PrintWriter pw;
+		public Chatserver(){
+			try{
+				ss = new ServerSocket(6789);
+				chatbox.append("\nWaiting for connection...");
+				s = ss.accept();   //blocking line 
+				players_in_room++;
+				chatbox.append("\nConnection established!");
+				br = new BufferedReader( new InputStreamReader(s.getInputStream()));
+				pw = new PrintWriter(s.getOutputStream());
+				
+				br.readLine();
+				
+				pw.println("thanks for connecting");
+				pw.flush();
+				String line = br.readLine();
+				while(line != null){
+					chatbox.append("\nclient: " + line);
+					br.readLine();
+				}//end of while still taking lines from client	
+				pw.close();
+				br.close();
+				s.close();
+				ss.close();
+			}catch(IOException ioe){
+				System.out.println("IOE in chatserver constructor: " + ioe.getMessage());
+			}
+			//end of try
+		}//end of constructor
+		
+	}//end of chat server class
+	
+	public class Chatclient extends Thread{
+		private PrintWriter pw;
+		private Socket s;
+		private BufferedReader br;
+		private String IPAddress;
+		
+		public Chatclient(String IPAddress){
+			this.IPAddress = IPAddress;
+			System.out.println("inside chatclient constructor");
+			try{
+				s = new Socket("localhost", 6789);
+				br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+				pw = new PrintWriter(s.getOutputStream());
+				this.start();
+				//Scanner keyboard = new Scanner(System.in);
+				
+				String  line = br.readLine();
+				while(line != null){
+					chatbox.append("From host: " + line);
+					line = br.readLine();
+				}//end of while	
+				String str = typefield.getText();
+				str = player.getName() + ": " + str;
+				pw.println(str);
+				pw.flush();
+				
+			}catch(IOException ioe){
+				System.out.println("IOE in chatclient constructor: " + ioe.getMessage());
+			} finally{
+				try{
+					pw.close();
+					br.close();
+					s.close();
+				}catch(IOException ioe){
+					System.out.println("IOE in chatclient finally block: " + ioe.getMessage());
+				}
+			}//end of finally block
+		
+		}//end of constructor
+		
+		public void run(){
+			Scanner keyboard = new Scanner(System.in);
+			String line = typefield.getText();
+			while(line != null){
+				//line = keyboard.nextLine();
+				line = player.getName() + ": " + line;
+				pw.println(line);
+				pw.flush();
+				line = typefield.getText();
+			}//end of line
+		}//end of run()
+	}//end of class
 }//end of class
+
--- CSCI201_FinalProject/src/main/Main.java
+++ CSCI201_FinalProject/src/main/Main.java
@@ -8,12 +8,14 @@ public class Main {
 		//Ted's test
 		//System.out.println("change");
 		//System.out.println("unchange");
-		//Player p1 = new Player("host");
+		Player p1 = new Player("host");
 		//GameLobbyGUI glg = new GameLobbyGUI(p1);
 		//glg.show(true);
-		//new GameRoomGUI(p1);
+		//Player p2 = new Player("player");
+		//GameRoomGUI grg1 = new GameRoomGUI(p1, true, "localhost");
+		//GameRoomGUI grg2 = new GameRoomGUI(p2, false, grg1.GetIPAddress());
 		//new PostGameGUI();
-		new GameScreenGUI();
+		//new GameScreenGUI();
 		
 	}
 
