149f961e11ecbfcfd8f310ec0028376396a457d1
danielnn <danielnn@usc.edu>
2015-04-08 20:09:20 -0700

    GameLobbyGUI - Daniel

--- CSCI201_FinalProject/src/main/GameLobbyGUI.java
+++ CSCI201_FinalProject/src/main/GameLobbyGUI.java
@@ -1,27 +1,49 @@
-package main;
-
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.GroupLayout.Alignment;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
-public class GameLobbyGUI extends JFrame{
-	private Player player;
-	private JLabel playerLabel;
+
+public class GameLobbyGUI extends JFrame {
+	private JTabbedPane mainPane;
+	private TabPanel easyPanel;
+	private TabPanel mediumPanel;
+	private TabPanel hardPanel;
 	
-	public GameLobbyGUI(Player player){
-		super("Game Lobby");
-		setLayout(new BorderLayout());		
-		setSize(900,600);
-		setLocation(100, 50);
+	public GameLobbyGUI(){
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int width = (int)screenSize.getWidth();
+		int height = (int)screenSize.getHeight();
+		setSize(600, 600);
+		setLocation (0, 0);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		this.player = player;
-		playerLabel = new JLabel(player.getName());
-		playerLabel.setAlignmentX(CENTER_ALIGNMENT);
-		add(playerLabel, BorderLayout.NORTH);
-		
+		initializeComponents();
+		createGUI();
 		setVisible(true);
-	}//end of constructor
-
-}//end of gamelobby gui class
+	}
+	
+	private void initializeComponents(){
+		mainPane = new JTabbedPane();
+		easyPanel = new TabPanel();
+		mediumPanel = new TabPanel();
+		hardPanel = new TabPanel();
+	}
+	
+	private void createGUI(){
+		setLayout(new BorderLayout());
+		
+		getContentPane().add(mainPane);
+		mainPane.add("EASY",easyPanel);
+		mainPane.add("MEDIUM", mediumPanel);
+		mainPane.add("HARD", hardPanel);
+		
+	}
+	
+	public static void main(String [] args){
+		new GameLobbyGUI();
+	}
+}
