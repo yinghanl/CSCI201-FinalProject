a3356b17e1540947107dd51d26bf2fb44195b0ea
danielnn <danielnn@usc.edu>
2015-04-08 19:46:53 -0700

    Daniel GameLobbyGUI

---
+++ CSCI201_FinalProject/src/main/TabPanel.java
@@ -0,0 +1,72 @@
+package main;
+import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class TabPanel extends JPanel {
+	JTable gameListTable;
+	
+	JPanel buttonPanel;
+	JButton createButton;
+	JButton joinButton;
+	JButton returnButton;
+	
+	
+	public TabPanel(){
+		initializeComponents();
+		createGUI();
+	}
+	
+	public void initializeComponents(){
+		buttonPanel = new JPanel();
+		createButton = new JButton("Create Game");
+		joinButton = new JButton("Join Game");
+		returnButton = new JButton("Return Button");
+		
+		gameListTable = new JTable(30,3);
+	}
+	
+	public void createGUI(){
+		//setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
+		//setLayout(new GridBagLayout());
+		
+		buttonPanel.setLayout(new BoxLayout(buttonPanel,BoxLayout.X_AXIS));
+		buttonPanel.add(joinButton);
+		buttonPanel.add(Box.createGlue());
+		buttonPanel.add(createButton);
+		buttonPanel.add(Box.createGlue());
+		buttonPanel.add(returnButton);
+		
+		JScrollPane jsp = new JScrollPane(gameListTable);
+		gameListTable.setFillsViewportHeight(true);
+		
+		add(jsp,BorderLayout.CENTER);
+		add(buttonPanel,BorderLayout.SOUTH);
+		
+		/*
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.gridheight = 10;
+		gbc.gridwidth = 3;
+		add(jsp,gbc);
+		
+		gbc.gridheight = 1;
+		gbc.gridx = 0;
+		gbc.gridy = 11;
+		
+		
+		add(buttonPanel,gbc);
+		*/
+		//add(jsp);
+		//add(buttonPanel);
+	}
+}
