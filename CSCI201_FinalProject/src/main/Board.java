6ca402ca6c07298faed7e8ac992b79ea125aab64
Ted Monyak <monyak@usc.edu>
2015-04-20 20:00:29 -0700

    unable to move over towers, can move over the path

--- CSCI201_FinalProject/src/main/Board.java
+++ CSCI201_FinalProject/src/main/Board.java
@@ -40,131 +40,57 @@ public class Board {
 	private void setAdjacencies(){
 		for(int i = 0; i<20; i++){
 			for(int j = 0; j<32; j++){
-				if(!(map[i][j] instanceof PathSpace)){
 					//north
-					if(i != 0 && !(map[i-1][j] instanceof PathSpace))
+					if(i != 0)
 						map[i][j].setNorth(map[i-1][j]);
 					else
 						map[i][j].setNorth(null);
 					
 					//south
-					if(i != 19 && !(map[i+1][j] instanceof PathSpace))
+					if(i != 19)
 						map[i][j].setSouth(map[i+1][j]);
 					else
 						map[i][j].setSouth(null);
 					
 					//west
-					if(j != 0 && !(map[i][j-1] instanceof PathSpace))
+					if(j != 0)
 						map[i][j].setWest(map[i][j-1]);
 					else
 						map[i][j].setWest(null);
 					
 					//east
-					if(j != 31 && !(map[i][j+1] instanceof PathSpace))
+					if(j <30 )
 						map[i][j].setEast(map[i][j+1]);
 					else
 						map[i][j].setEast(null);
 					
 					//northwest
-					if(i != 0 && j != 0 && !(map[i-1][j-1] instanceof PathSpace))
+					if(i != 0 && j != 0)
 						map[i][j].setNorthWest(map[i-1][j-1]);
 					else
 						map[i][j].setNorthWest(null);
 					
 					//northeast
-					if(i != 0 && j != 31 && !(map[i-1][j+1] instanceof PathSpace))
+					if(i != 0 && j != 31)
 						map[i][j].setNorthEast(map[i-1][j+1]);
 					else
 						map[i][j].setNorthEast(null);
 					
 					//southeast
-					if(i != 19 && j != 31 && !(map[i+1][j+1] instanceof PathSpace))
+					if(i != 19 && j != 31)
 						map[i][j].setSouthEast(map[i+1][j+1]);
 					else
 						map[i][j].setSouthEast(null);
 					
 					//southwest
-					if(i != 19 && j != 0 && !(map[i+1][j-1] instanceof PathSpace))
+					if(i != 19 && j != 0)
 						map[i][j].setSouthWest(map[i+1][j-1]);
 					else
-						map[i][j].setSouthWest(null);
-				}
+						map[i][j].setSouthWest(null); 
 
 			}
 		}
 	}
-	
-
-/*	
-	private void getCreepPath()
-	{
-		creepPath = new ArrayList<PathSpace>();
-		creepPath.add(new PathSpace(14,0));
-		creepPath.add(new PathSpace(14,1));
-		creepPath.add(new PathSpace(14,2));
-		creepPath.add(new PathSpace(14,3));
-		creepPath.add(new PathSpace(14,4));
-
-		creepPath.add(new PathSpace(13,4));
-		creepPath.add(new PathSpace(12,4));
-		creepPath.add(new PathSpace(11,4));
-		creepPath.add(new PathSpace(10,4));
-		creepPath.add(new PathSpace(9,4));
-		creepPath.add(new PathSpace(8,4));
-		creepPath.add(new PathSpace(7,4));
-		creepPath.add(new PathSpace(6,4));
-		creepPath.add(new PathSpace(5,4));
-
-		creepPath.add(new PathSpace(5,5));
-		creepPath.add(new PathSpace(5,6));
-		creepPath.add(new PathSpace(5,7));
-		creepPath.add(new PathSpace(5,8));
-		creepPath.add(new PathSpace(5,9));
-		creepPath.add(new PathSpace(5,10));
-		creepPath.add(new PathSpace(5,11));
-
-		creepPath.add(new PathSpace(6,11));
-		creepPath.add(new PathSpace(7,11));
-		creepPath.add(new PathSpace(8,11));
-		creepPath.add(new PathSpace(9,11));
-		creepPath.add(new PathSpace(10,11));
-		creepPath.add(new PathSpace(11,11));
-		creepPath.add(new PathSpace(12,11));
-		creepPath.add(new PathSpace(13,11));
-		creepPath.add(new PathSpace(14,11));
-		creepPath.add(new PathSpace(15,11));
-		creepPath.add(new PathSpace(16,11));
-
-		creepPath.add(new PathSpace(16,12));
-		creepPath.add(new PathSpace(16,13));
-		creepPath.add(new PathSpace(16,14));
-		creepPath.add(new PathSpace(16,15));
-		creepPath.add(new PathSpace(16,16));
-		creepPath.add(new PathSpace(16,17));
-		creepPath.add(new PathSpace(16,18));
-		creepPath.add(new PathSpace(16,19));
-		creepPath.add(new PathSpace(16,20));
-		
-		creepPath.add(new PathSpace(15,20));
-		creepPath.add(new PathSpace(14,20));
-		creepPath.add(new PathSpace(13,20));
-		creepPath.add(new PathSpace(12,20));
-		creepPath.add(new PathSpace(11,20));
-		creepPath.add(new PathSpace(10,20));
-
-		creepPath.add(new PathSpace(10,21));
-		creepPath.add(new PathSpace(10,22));
-		creepPath.add(new PathSpace(10,23));
-		creepPath.add(new PathSpace(10,24));
-		creepPath.add(new PathSpace(10,25));
-		creepPath.add(new PathSpace(10,26));
-		creepPath.add(new PathSpace(10,27));
-		creepPath.add(new PathSpace(10,28));
-		creepPath.add(new PathSpace(10,29));
-		creepPath.add(new PathSpace(10,30));
-		creepPath.add(new PathSpace(10,31));
-	}
-*/
 	
 	private void setPathSpaces(){
 		for(int i = 0; i<creepPath.size()-1; i++){
@@ -186,5 +112,18 @@ public class Board {
 	public void placeTower(int x, int y)
 	{
 		map[x][y] = new TowerSpace(x, y);
+		if(x>0){
+			map[x-1][y].setSouth(null);
+		}
+		if(y>0){
+			map[x][y-1].setEast(null);
+		}
+		if(y<31){
+			map[x][y+1].setWest(null);
+		}
+		if(x<19){
+			map[x+1][y].setNorth(null);
+		}
+		
 	}
 }
