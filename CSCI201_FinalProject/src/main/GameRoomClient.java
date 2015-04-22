b0dff4ada0347a3b1a967b575aaa59a6788e3f63
Griffin Sloves <gsloves@usc.edu>
2015-04-21 21:41:53 -0700

    Changing GameRoomServer and created GameRoomClient

---
+++ CSCI201_FinalProject/src/main/GameRoomClient.java
@@ -0,0 +1,63 @@
+package main;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Vector;
+
+public class GameRoomClient extends Thread
+{
+
+	private TabPanel tp;
+	private Socket s;
+	private ObjectInputStream ois;
+	private ObjectOutputStream oos;
+	
+	public GameRoomClient(TabPanel tp)
+	{
+		this.tp = tp;
+		try 
+		{
+			s = new Socket("localhost", 8000); //should take in IP address of host
+			System.out.println("Client Connecting");
+			ois = new ObjectInputStream(s.getInputStream());
+			oos = new ObjectOutputStream(s.getOutputStream());
+			this.start();
+		}
+		
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+		}
+		finally {
+			try{
+				System.out.println("Closing everything");
+				oos.close();
+				ois.close();
+				s.close();
+			} catch (IOException ioe) { ioe.printStackTrace(); }
+		}
+	
+	}
+	
+	public void run()
+	{
+		try
+		{
+			while(true)
+			{
+				Vector<Game> gamesOpen = (Vector<Game>)ois.readObject();
+				tp.updateGames(gamesOpen);
+			}
+		}
+		catch(ClassNotFoundException cnfe)
+		{
+			System.out.println("CNFE: " + cnfe.getMessage());
+		}
+		catch(IOException ioe)
+		{
+			System.out.println("IOE: " + ioe.getMessage());
+		}
+	}
+}
