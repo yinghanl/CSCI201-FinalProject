4e1cb77534eb818dab31fefdb5540556857d47c2
Yinghan Lei <yinghanl@usc.edu>
2015-04-25 23:09:22 -0700

    Synched gold counters

--- CSCI201_FinalProject/src/main/GameRoomGUI.java
+++ CSCI201_FinalProject/src/main/GameRoomGUI.java
@@ -228,7 +228,7 @@ public class GameRoomGUI extends JFrame {
 //				GameScreenGUI gs = new GameScreenGUI(b, user.toPlayer(), true);
 //				gs.setVisible(false);
 				b.setPlayer(p);
-				new GameScreenGUI(b, p, true);
+				new GameScreenGUI(b, p, true, null);
 				sendMessageToClients(new Integer(-1));
 				grc.deleteGame();
 				setVisible(false);
@@ -556,7 +556,7 @@ public class GameRoomGUI extends JFrame {
 								Board b = new Board();
 								Player p = new Player(user.getUsername(), b.getSpace(19,30));
 								b.setPlayer(p);
-								new GameScreenGUI(b, p, false);
+								new GameScreenGUI(b, p, false, null);
 								setVisible(false);
 							}
 							else if((Integer)obj == -3)
